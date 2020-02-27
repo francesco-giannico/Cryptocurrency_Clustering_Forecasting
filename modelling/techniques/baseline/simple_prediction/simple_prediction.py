08e62839ba425003320de368fe54af6b3124c00e
Francesco Giannico <frankgiak@gmail.com>
2020-02-27 15:10:43 +0100

    - Simple prediction corrected

--- modelling/techniques/baseline/simple_prediction/simple_prediction.py
+++ modelling/techniques/baseline/simple_prediction/simple_prediction.py
@@ -1,4 +1,7 @@
+import calendar
 import os
+from datetime import timedelta, datetime
+
 import pandas as pd
 import numpy as np
 from math import sqrt
@@ -8,8 +11,8 @@ from modelling.techniques.forecasting.evaluation.error_measures import get_rmse
 from utility.folder_creator import folder_creator
 
 result_folder="../modelling/techniques/baseline/simple_prediction/output/"
-partial_folder="crypto"
-final_folder="RMSE"
+partial_folder="predictions"
+final_folder="average_rmse"
 
 date=[]
 def simple_prediction(data_path,test_set):
@@ -17,24 +20,29 @@ def simple_prediction(data_path,test_set):
     folder_creator(result_folder+final_folder,1)
 
     for crypto in os.listdir(data_path):
-        df= pd.read_csv(data_path+crypto,usecols=['Date','Open','Close'])
+        df= pd.read_csv(data_path+crypto,usecols=['Date','Close'])
 
         #new dataframe for output
         df1=pd.DataFrame(columns=["date","observed_value","predicted_value"])
-        for date in test_set:
-            row_of_interest=df[df['Date']==date]
-            row_of_interest=row_of_interest.set_index('Date')
-            df1 = df1.append({'date':date,'observed_value':row_of_interest.loc[date,'Open'],'predicted_value':row_of_interest.loc[date,'Close']},ignore_index=True)
+        for date_to_predict in test_set:
+            day_before = (pd.to_datetime(date_to_predict,format="%Y-%m-%d") - timedelta(days=1)).strftime('%Y-%m-%d')
+
+            row_day_before=df[df['Date']==day_before]
+            row_day_before = row_day_before.set_index('Date')
+
+            row_day_to_predict=df[df['Date']==date_to_predict]
+            row_day_to_predict=row_day_to_predict.set_index('Date')
+
+            df1 = df1.append({'date':date_to_predict,'observed_value':row_day_to_predict.loc[date_to_predict,'Close'],'predicted_value':row_day_before.loc[day_before,'Close']},ignore_index=True)
         df1.to_csv(result_folder+partial_folder+"/"+crypto,sep=",",index=False)
 
-    #RMSE, ma forse è errato!!
     rmses=[]
     for crypto in os.listdir(result_folder+partial_folder+"/"):
         df = pd.read_csv(result_folder+partial_folder+"/"+crypto)
         #get rmse for each crypto
         rmses.append(get_rmse(df['predicted_value'],df['observed_value']))
 
-    with open(os.path.join(result_folder,final_folder,"RMSE.txt"), 'w+') as out:
+    with open(os.path.join(result_folder,final_folder,"average_rmse.txt"), 'w+') as out:
         final = np.mean(rmses)
         out.write(str(final))
 
